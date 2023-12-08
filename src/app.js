bd91e58d39cdd3fd84685f091b7166623dce0dec
Thasnima Begum <thasnima@hotmail.co.uk>
2023-12-08 22:22:50 +0000

    subtract test done

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 const stringFunc = require('./lib/strings');
+const numberFunc = require('./lib/numbers');
 
 app.get('/strings/hello/world', (req, res) => {
   res.status(200).send({ result: 'Hello, world!' });
@@ -30,6 +31,24 @@ app.get('/strings/count-characters/:string', (req, res) => {
 
 app.get('/strings/first-characters/:string', (req, res) => {
   res.status(200).json({ result: stringFunc.firstCharacters(req.params.string, req.query.length) });
+});
+
+app.get('/numbers/add/:num1/and/:num2', (req, res) => {
+  const a = parseInt(req.params.num1, 10);
+  const b = parseInt(req.params.num2, 10);
+  if (Number.isNaN(a) || Number.isNaN(b)) {
+    return res.status(400).send({ error: 'Parameters must be valid numbers.' });
+  }
+  return res.status(200).send({ result: numberFunc.add(a, b) });
+});
+
+app.get('/numbers/subtract/:num2/from/:num1', (req, res) => {
+  const a = parseInt(req.params.num1, 10);
+  const b = parseInt(req.params.num2, 10);
+  if (Number.isNaN(a) || Number.isNaN(b)) {
+    return res.status(400).send({ error: 'Parameters must be valid numbers.' });
+  }
+  return res.status(200).send({ result: numberFunc.subtract(a, b) });
 });
 
 module.exports = app;
